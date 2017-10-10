b5c64d17e737d1dfc7b4aa893b94f656e60228c1
Сергей <esinsergej@gmail.com>
2017-10-10 20:41:32 +0300

    Lesson 4

---
+++ Lesson_04_Ef_Else/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_04_Ef_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6, b = 6;
+
+            if (a<b)
+            {
+                Console.WriteLine("a больше b");
+            }
+            else if (a>b)
+            {
+                Console.WriteLine("a меньше b");
+             
+            }
+            else
+        	{
+
+                Console.WriteLine("a навно b");
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
