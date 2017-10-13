b2819ecc8569ff6fe45fc6e58318e2ec09631b6b
Сергей <esinsergej@gmail.com>
2017-10-13 15:11:08 +0300

    Массивы

---
+++ Lesson_MS_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class LINQQueryExpressions
+{
+    static void Main()
+    {
+
+        // Specify the data source.
+        //int[] scores = new int[] { 97, 92, 81, 60, 62, 33, 24, 80, 99, 2, 0, 1, 5 };
+        int[] scores = { 97, 92, 81, 60, 62, 33, 24, 80, 99, 2, 0, 1, 5 };
+
+        // Define the query expression.
+        IEnumerable<int> scoreQuery =
+            from score in scores
+            where score > 1
+            select score;
+
+        // Execute the query.
+        foreach (int i in scoreQuery)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.ReadKey();
+    }
+}
