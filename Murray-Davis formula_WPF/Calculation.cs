6b46e0df514e251acb44ddd166c3bc27c9122f2d
Сергей <esinsergej@gmail.com>
2017-10-20 22:03:49 +0300

    Split

---
+++ Murray-Davis formula_WPF/Calculation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Murray_Davis_formula_WPF
+{
+    public class Calculation
+    {
+
+        public void TestF()
+        {
+
+            MessageBox.Show("ha ha");
+
+        }
+
+        public void TestF(int a, int b)
+        {
+            int c = 10;
+            c = +a / b;
+            MessageBox.Show("C равно " + c);
+
+        }
+
+        public double MDev(double a, double b)
+        {
+
+            double c = 100 * ((1 - Math.Pow(10, -b)) / (1 - Math.Pow(10, -a)));
+
+            return (c);
+
+            //MessageBox.Show("Растровая плашка равна " + c + "%");
+
+        }
+
+
+    }
+}
---
+++ Murray-Davis formula_WPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Murray_Davis_formula_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        Calculation MyCalc = new Calculation();
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MyCalc.TestF();
+            //MyCalc.TestF(1, 3);
+            //MyCalc.MDev(1.457, 0.48);
+
+            double MyRezalt = MyCalc.MDev(double.Parse(tb_Ds.Text), double.Parse(tb_Dr.Text));
+
+
+            tb_Dot.Text = string.Concat(MyRezalt);
+            //tb_Dot.Text = string.Concat(MyCalc.MDev(1.457, 0.48));
+
+
+            //MessageBox.Show(string.Concat(MyRezalt));
+
+        }
+    }
+}
