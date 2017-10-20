a0231c4a8b5590ade470acb5f3f1ba824b9b9364
Сергей <esinsergej@gmail.com>
2017-10-20 22:03:49 +0300

    Split

--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -11,10 +11,34 @@ namespace CSharp_Lesson_01
         static void Main(string[] args)
         {
 
-            Console.Write("Hello world!");
+            //Console.Write("Hello world!");
+            //Console.ReadKey();
+            //Console.Write("My first program");
+            //Console.ReadKey();
+
+            //double a = double.Parse("1,54");
+
+            //Console.WriteLine(a);
+
+            //Console.ReadKey();
+
+
+            string words = "C 0,130 0,042 0,016 0,088 " + 
+                           "C 0,062 0,021 0,010 0,043 " +
+                           "K 1,665 1,803 2,016 1,729 ";
+
+            string[] split = words.Split(new Char[] { ' '/*, ',', '.', ':', '\t' */});
+
+            foreach (string s in split)
+            {
+
+                if (s.Trim() != "")
+                    Console.WriteLine(s);
+
+
+            }
+
             Console.ReadKey();
-            Console.Write("My first program");
-            Console.ReadKey();    
         }
     }
 }
