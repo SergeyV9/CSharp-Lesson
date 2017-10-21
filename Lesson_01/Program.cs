30e715eb3be09708bc68dff9485bcaf7f6b13173
Сергей <esinsergej@gmail.com>
2017-10-22 02:14:33 +0300

    Read file

--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp_Lesson_01
 {
@@ -11,32 +7,14 @@ namespace CSharp_Lesson_01
         static void Main(string[] args)
         {
 
-            //Console.Write("Hello world!");
-            //Console.ReadKey();
-            //Console.Write("My first program");
-            //Console.ReadKey();
+            Console.Write("Hello world!");
+            Console.ReadKey();
+            Console.Write("My first program");
+            Console.ReadKey();
 
-            //double a = double.Parse("1,54");
+            double a = double.Parse("1,54");
 
-            //Console.WriteLine(a);
-
-            //Console.ReadKey();
-
-
-            string words = "C 0,130 0,042 0,016 0,088 " + 
-                           "C 0,062 0,021 0,010 0,043 " +
-                           "K 1,665 1,803 2,016 1,729 ";
-
-            string[] split = words.Split(new Char[] { ' '/*, ',', '.', ':', '\t' */});
-
-            foreach (string s in split)
-            {
-
-                if (s.Trim() != "")
-                    Console.WriteLine(s);
-
-
-            }
+            Console.WriteLine(a);
 
             Console.ReadKey();
         }
---
+++ Read_file/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Read_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] spline = File.ReadAllLines("C:\\1.txt");
+            string[,] myresut = new string[spline.Length, 5];
+            string mywords = "";
+
+            for (int i = 0; i < spline.Length; i++)
+            {
+                mywords = spline[i];
+                string[] split = mywords.Split(new Char[] { ' '/*, ',', '.', ':', '\t' */});
+
+                for (int j = 0; j < 5; j++)
+                {
+                    myresut.SetValue(split[j], i, j);
+                    Console.WriteLine(myresut[i, j]);
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
