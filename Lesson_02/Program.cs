e1d04da4fea109f2b52745ecddcea8c702d3caf2
Сергей <esinsergej@gmail.com>
2017-11-02 03:01:30 +0300

    Class

--- Lesson_02/Program.cs
+++ Lesson_02/Program.cs
@@ -10,6 +10,52 @@ namespace Lesson_02
     {
         static void Main(string[] args)
         {
+            Myclacc myclacc = new Myclacc();
+            MyBoock boock = new MyBoock();
+
+            //myclacc.Sos();
+            Console.WriteLine("Переменная класса " + myclacc.Id);
+            boock.ID = 1000;
+            string G = boock.ToString();
+            Console.WriteLine("Переменная " + boock.ToString() + " = " + boock.ID);
+
+            Console.ReadKey();
+
+        }
+    }
+
+
+    class Myclacc
+    {
+        public int Id { get; set; } = 33;
+
+        //public void Sos()
+        //{
+        //    Console.WriteLine("Метод класса");
+        //}
+    }
+
+
+    class MyBoock
+    {
+       private int id;
+       
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                if(value > 0)
+                {
+                    id = value / 2;
+                }
+
+            }
         }
     }
 }
